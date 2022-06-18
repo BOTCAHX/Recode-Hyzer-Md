05806d69ec27cb2be5e2cfc6cf07cd5fa5867b87
botcahxx <107209070+botcahxx@users.noreply.github.com>
2022-06-19 04:18:01 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,20 +1,10 @@
-/*
- * ngelarang lu biar g diubah kyknya emg susah ya anjg
- * jadi kalau lu g bsa ngehargain orang
- * setidaknya buat sc ori by gw, trs buat sc lu hasil copas tuh
- * better bgtu dripada nma gw dihapus smua
- * najis 👎🗿
-*/
-
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
 Hai ${ye} Lagi Nyari Sc Ya? 
 
 Cari aja sendiri 😞
-
-
-_*Jangan Lupa Di Subscribe*_`
+`
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
 handler.help = ['sc', 'sourcecode']
