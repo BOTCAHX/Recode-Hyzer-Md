f3635f144ee25950da7a6d55cd039bd6dc310952
botcahxx <107209070+botcahxx@users.noreply.github.com>
2022-06-18 11:01:47 +0700

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -14,7 +14,7 @@ hyzer = {
         }
        } 
      }
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg', hyzer, { packname: "sticker by", author: "BOT" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg', hyzer, { packname: "Bot", author: "TioXd" })
 }
 
 handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
