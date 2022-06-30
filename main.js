217b390313f213cf4aa414cfa8ccd98aa6064b4c
penyumbat <107585175+penyumbat@users.noreply.github.com>
2022-06-30 19:02:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -124,8 +124,8 @@ global.reloadHandler = function (restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate)
   }
 
-  conn.welcome = 'Yahh! Beban nya nambah deh:(\nSelamat datang wahai Beban, di grup @subject\n\n@desc'
-  conn.bye = 'Sipp! Beban Berkurang satu'
+  conn.welcome = 'Hai @user Selamat Datang\nDi Grup @subject'
+  conn.bye = 'Member Berkurang satu @user'
   conn.spromote = '@user sekarang admin!'
   conn.sdemote = '@user sekarang bukan admin!'
   conn.handler = handler.handler.bind(conn)
