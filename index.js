f245984eb387be7f7eed093755fb85e1938405f6
TioXd <94074076+BOTCAHX@users.noreply.github.com>
2022-07-01 23:39:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('BOT BY\n Helga', {
+CFonts.say('BOT BY\n Tio', {
   colors: ['blueBright','yellowBright'],                                        font: 'block',
   align: 'center',
 })
-CFonts.say(`BY Helga Zexs`, {                                            
+CFonts.say(`BY TioXd`, {                                            
 colors: ['yellow'],                                                           
 font: 'console',                                                              align: 'center',
 })
